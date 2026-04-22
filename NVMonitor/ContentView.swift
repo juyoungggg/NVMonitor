1ce62425648aac47a7f20cd8da86ba9dd68436ba
juyoungggg <eun9455@gmail.com>
2026-04-23 03:55:34 +0900

    Initial Commit

---
+++ NVMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NVMonitor
+//
+//  Created by 선주영 on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NVMonitor/NVMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVMonitorApp.swift
+//  NVMonitor
+//
+//  Created by 선주영 on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct NVMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
